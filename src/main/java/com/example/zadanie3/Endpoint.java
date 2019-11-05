ac2e3df41e38e42f21eec784a24cbc1509541d07
209305 <Infa123!>
2019-11-05 16:58:36 +0100

    Zadanie3

--- src/main/java/com/example/zadanie3/Endpoint.java
+++ src/main/java/com/example/zadanie3/Endpoint.java
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
